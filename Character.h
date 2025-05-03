c6f341c501bdf1a371bd0a061301075aa3b71790
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-05-03 22:55:32 +0500

    Update Character.h
    Added default and parameterized constructors and destructors

--- Character.h
+++ Character.h
@@ -10,23 +10,29 @@ private:
 	int attack;
 	int defense;
 public:
-	void getName() {
-
-	}
+	Character();
+	Character(string n, int l, int h, int a, int d);
+	virtual ~Character() {}
 	virtual void attackTarget(Character& c) = 0;
 	virtual void useSpecialAbility() = 0;
 };
 class Warrior : public Character {
 public:
+	Warrior() : Character("Warrior", 1, 100, 10, 50) {}
+	void attackTarget(Character& c);
 	void useSpecialAbility();
 };
 class Mage : public Character {
 public:
+	Mage() : Character("Mage", 1, 85, 15, 25) {}
+	void attackTarget(Character& c);
 	void useSpecialAbility();
 };
 class Archer : public Character {
 public:
-	void useSpecialAbility();
+	Archer() : Character("Archer", 1, 75, 20, 10) {}
+	void attackTarget(Character& c);
+	void useSpecialAbility() override;
 };
 
 #endif // !CHARACTER_H
