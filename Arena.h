173cff9be4dde5df28c5794b6c15fdf843c624cd
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-05-04 02:21:46 +0500

    Create Arena.h
    Arena class with different environments and 2 characters

---
+++ Arena.h
@@ -0,0 +1,27 @@
+#ifndef ARENA_H
+#define ARENA_H
+#include"Character.h"
+#include <iostream>
+using namespace std;
+
+enum class environmentType {FIRE, ICE, JUNGLE, DESERT};
+
+class Arena{
+private:
+    string name;
+    environmentType environment;
+    Character* c1;
+    Character* c2;
+public:
+    Arena();
+    Arena(string n, environmentType e, Character* c1, Character* c2);
+    
+    void applyEnvironmentEffects(); 
+    void triggerRandomWeather();   
+    void startBattle();                
+
+    string getEnvironmentName() const;
+    void resetWeatherEffects();
+};
+
+#endif
